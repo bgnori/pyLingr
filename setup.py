3c114f929268f56b997484e5ee880c2266aeb48e
Noriyuki Hosaka <nori@akagi.(none)>
2012-04-19 21:55:55 +0900

    modified:   pyLingr.egg-info/PKG-INFO 	modified:   pyLingr.egg-info/SOURCES.txt 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
+import os.path
+
+
+try:
+    # add classifiers and download_url syntax to distutils
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+except:
+    pass
+
+
+
 
 setup(
-  name='pyLingr',
-  version='0.0.1',
-  url='https://github.com/bgnori/pyLingr',
-  py_modules=['pylingr'],
+    name='pyLingr',
+    zip_safe=False,
+    version='0.0.1',
+    url='https://github.com/bgnori/pyLingr',
+    py_modules=['pylingr'],
+    provides=['pyLingr'],
 )
 
